7c9dece248121b44d0e9369a2cc06acc17d5177f
agungsuryaarie <agungbahari3007@gmail.com>
2023-06-22 13:31:25 +0700

    add

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,83 @@
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="widget">
+                    <h3 class="mb-4">About</h3>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
+                        live the blind texts.</p>
+                </div>
+                <div class="widget">
+                    <h3>Social</h3>
+                    <ul class="list-unstyled social">
+                        <li><a href="#"><span class="icon-instagram"></span></a></li>
+                        <li><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li><a href="#"><span class="icon-linkedin"></span></a></li>
+                        <li><a href="#"><span class="icon-pinterest"></span></a></li>
+                        <li><a href="#"><span class="icon-dribbble"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 ps-lg-5">
+                <div class="widget">
+                    <h3 class="mb-4">Company</h3>
+                    <ul class="list-unstyled float-start links">
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Vision</a></li>
+                        <li><a href="#">Mission</a></li>
+                        <li><a href="#">Terms</a></li>
+                        <li><a href="#">Privacy</a></li>
+                    </ul>
+                    <ul class="list-unstyled float-start links">
+                        <li><a href="#">Partners</a></li>
+                        <li><a href="#">Business</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Creative</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4">
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-12 text-center">
+                <p>Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>. All Rights Reserved. &mdash; Designed with love by <a
+                        href="https://untree.co">Untree.co</a> Distributed by <a
+                        href="https://themewagon.com">ThemeWagon</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Preloader -->
+<div id="overlayer"></div>
+<div class="loader">
+    <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+</div>
+
+
+
+
+<script src="{{ url('front-template/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ url('front-template/js/tiny-slider.js') }}"></script>
+<script src="{{ url('front-template/js/flatpickr.min.js') }}"></script>
+<script src="{{ url('front-template/js/aos.js') }}"></script>
+<script src="{{ url('front-template/js/glightbox.min.js') }}"></script>
+<script src="{{ url('front-template/js/navbar.js') }}"></script>
+<script src="{{ url('front-template/js/counter.js') }}"></script>
+<script src="{{ url('front-template/js/custom.js') }}"></script>
+
+
+</body>
+
+</html>
